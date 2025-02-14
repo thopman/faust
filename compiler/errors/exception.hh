thopman/faust

commit 5211c05fb2f01a252dff1c7f539680da075ce8bc

Change execinfo.h check to depend on __GLIBC__ being defined

compiler/errors/exception.hh
@@ -30,7 +30,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 // #include <io.h>
 #endif
 
-#if !defined(EMCC) && !defined(WIN32) && !defined(ANDROID) && !defined(ALPINE)
+#ifdef __GLIBC__
 #include <execinfo.h>
 #endif
 
@@ -58,7 +58,7 @@ class faustexception : public std::runtime_error {
 
 inline void stacktrace(std::stringstream& str, int val)
 {
-#if !defined(EMCC) && !defined(WIN32) && !defined(ANDROID) && !defined(ALPINE)
+#ifdef __GLIBC__
     void** callstack = (void**)alloca(val * sizeof(void*));
     int    frames    = backtrace(callstack, val);
     char** strs      = backtrace_symbols(callstack, frames);

commit 8c91268364ba3d36bb2541e85d0ef7b12b6c6825

stacktrace was actually broken after 5211c05fb2f01a252dff1c7f539680da075ce8bc.

compiler/errors/exception.hh
@@ -30,7 +30,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 // #include <io.h>
 #endif
 
-#ifdef __GLIBC__
+#if defined(__GLIBC__) || defined(__APPLE__)
 #include <execinfo.h>
 #endif
 
@@ -58,7 +58,7 @@ class faustexception : public std::runtime_error {
 
 inline void stacktrace(std::stringstream& str, int val)
 {
-#ifdef __GLIBC__
+#if defined(__GLIBC__) || defined(__APPLE__)
     void** callstack = (void**)alloca(val * sizeof(void*));
     int    frames    = backtrace(callstack, val);
     char** strs      = backtrace_symbols(callstack, frames);